415df12f969a82587331e3e1dd78bdf043867661
Gautier Berthou <gautier.berthou@outlook.fr>
2020-10-03 19:33:24 +0200

    Custom OpenSimplex noise

---
+++ noise.py
@@ -0,0 +1,9 @@
+from opensimplex import OpenSimplex
+
+class Noise:
+    def __init__(self, zoom, seed = 0):
+        self.noise = OpenSimplex(seed)
+        self.zoom = zoom
+
+    def get(self, X, Y):
+        return self.noise.noise2d(x = (X * self.zoom), y = (Y * self.zoom))
